fc1f7eaa147d151570e2437dab27db33df57dfed
amenslama30 <amenslama25@gmail.com>
2024-05-30 15:53:47 +0100

    Your descriptive commit message here

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -2,7 +2,14 @@ const { defineConfig, devServer } = require('@vue/cli-service')
 const fs = require('fs');
 const path = require('path');
 
-module.exports = {
-  outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-}
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: 'all',
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'server.cert')),
+    },
+  }
+})
+
